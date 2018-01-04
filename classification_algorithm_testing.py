bc493203183ffa342498597d9a946d464a581853
acjones1987 <aaronc.jones@me.com>
2018-01-03 21:35:55 -0800

    adding classification files

--- classification_algorithm_testing.py
+++ classification_algorithm_testing.py
@@ -1,15 +1,15 @@
 """
 Author: Aaron Jones
-Date: 2017-11-24
+Date: 2017-12-09
 """
 
 ### Loading Libraries ----------
 import pandas as pd
 import numpy as np
-
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
-from sklearn.metrics import confusion_matrix, precision_recall_curve, roc_curve
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.dummy import DummyClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
@@ -18,152 +18,133 @@ from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
 
+execfile('classification_algorithm_testing_functions.py')
+
 ### Loading Data From UC Irvine Machine Learning Repository - Blood Transfusion Data Set ----------
 blood = pd.read_table('http://archive.ics.uci.edu/ml/machine-learning-databases/blood-transfusion/transfusion.data'
                       , sep = ','
                       , header = 0
                       , names = ['Recency', 'Frequency', 'Monetary', 'Time', 'March2007'])
 
-print blood.head()
-print blood.shape
-print blood.March2007.value_counts()
+### Plots ----------
+colormap = plt.cm.RdBu
+plt.figure(figsize = (14,12))
+plt.title('Pearson Correlation of Features', y = 1.05, size = 15)
+sns.heatmap(blood.astype(float).corr(), linewidths = 0.1, vmax = 1.0, square = True, cmap = colormap, linecolor = 'white', annot = True)
 
-### Splitting The Data ----------
-X = blood.drop('March2007', axis = 1)
-y = blood.March2007
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0, stratify = y)
+pairs = sns.pairplot(
+    blood[[u'March2007', u'Recency', u'Frequency', u'Monetary', u'Time']],
+    hue = 'March2007', palette = 'seismic', size = 1.2, diag_kind = 'kde', diag_kws = dict(shade = True), plot_kws = dict(s = 10)
+)
+pairs.set(xticklabels = [])
 
-### Scaling Data ----------
-minmax = MinMaxScaler()
-X_train_scaled = minmax.fit_transform(X_train)
-X_test_scaled = minmax.transform(X_test)
+### Define Major Values ----------
+SEED = 0
+NFOLDS = 5
 
-### Functions ----------
-def cmat(y_true, y_pred):
-    matrix = confusion_matrix(y_true, y_pred)
-    return matrix
+X_train, X_test, y_train, y_test, X_train_minmax, X_test_minmax, X_train_std, X_test_std = dataPrep(blood, 'March2007', SEED)
 
-def accuracy(y_true, y_pred):
-    matrix = cmat(y_true, y_pred)
-    #Accuary = (TP + TN) / (TP + TN + FN + FP)
-    acc = (matrix[0, 0] + matrix[1, 1]) / float(matrix[0, 0] + matrix[0, 1] + matrix[1, 0] + matrix[1, 1])
-    return acc
+### Results Tables ----------
+train_stats = []
+test_stats = []
 
-def sensitivity(y_true, y_pred):
-    matrix = cmat(y_true, y_pred)
-    #Sensitivity = TP / (FN + TP)
-    sens = matrix[1, 1] / float(matrix[1, 0] + matrix[1, 1])
-    return sens
+### Dummy ----------
+dummyGrid = {'strategy': ['most_frequent', 'stratified', 'uniform'],
+             'random_state': [SEED]}
 
-def specificity(y_true, y_pred):
-    matrix = cmat(y_true, y_pred)
-    #Specificity = TN / (TN + FP)
-    spec = matrix[0, 0] / float(matrix[0, 0] + matrix[0, 1])
-    return spec
+model_dummy, y_train_dummy, y_test_dummy = modelFit(DummyClassifier(),
+                                                    parameters=dummyGrid, whichXTrain=X_train, whichXTest=X_test, whichYTrain=y_train)
 
-def positive_predictive_value(y_true, y_pred):
-    matrix = cmat(y_true, y_pred)
-    #Positive Predictive Value = TP / (FP + TP)
-    ppv = matrix[1, 1] / float(matrix[0, 1] + matrix[1, 1])
-    return ppv
+dummy_train_stats = modelScoring(y_train, y_train_dummy, 'Training', 'Dummy', printing = False, table = train_stats)
+dummy_test_stats = modelScoring(y_test, y_test_dummy, 'Testing', 'Dummy', printing = False, table = test_stats)
 
-def negative_predictive_value(y_true, y_pred):
-    matrix = cmat(y_true, y_pred)
-    #Negative Predictive Value = TN / (TN + FN)
-    npv = matrix[0, 0] / float(matrix[0, 0] + matrix[1, 0])
-    return npv
+### Gaussian Naive Bayes ----------
+model_gnb, y_train_gnb, y_test_gnb = modelFit(GaussianNB(),
+                                              parameters=None, whichXTrain=X_train, whichXTest=X_test, whichYTrain=y_train)
 
-def model_results(model, which, X1 = X_train_scaled, X2 = X_test_scaled, y1 = y_train, y2 = y_test):
-    #roc curve, precision recall curve
-
-    train_score = model.score(X1, y1)
-    test_score = model.score(X2, y2)
-
-    y_train_predicted = model.predict(X1)
-    y_test_predicted = model.predict(X2)
-
-    confusion_matrix_train = cmat(y1, y_train_predicted)
-    confusion_matrix_test = cmat(y2, y_test_predicted)
-
-    results_dict = {'Algorithm': which
-        , 'Accuracy (Train, Test)': (round(accuracy(y1, y_train_predicted), 4),
-                                     round(accuracy(y2, y_test_predicted), 4))
-        , 'Sensitivity (Train, Test)': (round(sensitivity(y1, y_train_predicted), 4),
-                                        round(sensitivity(y2, y_test_predicted), 4))
-        , 'Specificity (Train, Test)': (round(specificity(y1, y_train_predicted), 4),
-                                        round(specificity(y2, y_test_predicted), 4))
-        , 'Positive Predictive Value (Train, Test)': (round(positive_predictive_value(y1, y_train_predicted), 4),
-                                                      round(positive_predictive_value(y2, y_test_predicted), 4))
-        , 'Negative Predictive Value (Train, Test)': (round(negative_predictive_value(y1, y_train_predicted), 4),
-                                                      round(negative_predictive_value(y2, y_test_predicted), 4))}
-
-    return train_score, test_score, confusion_matrix_train, confusion_matrix_test, results_dict
-
-### Dummy Classifier - Most Frequent ----------
-dum_mod = DummyClassifier(random_state = 0)
-dum_grid = {'strategy': ['most_frequent', 'stratified']}
-dum = GridSearchCV(dum_mod, dum_grid, scoring = 'accuracy')
-dum.fit(X_train_scaled, y_train)
-
-dum_train_score, dum_test_score, dum_train_confusion_matrix, dum_test_confusion_matrix, dum_results_dict = model_results(dum, 'Dummy')
-
-print 'Mean Training Set Accuracy (based on 5 fold) - Dummy: {}'.format(dum_train_score)
-print 'Test Set Accuracy - Dummy: {}'.format(dum_test_score)
-
-### Naive Bayes ----------
-gnb = GaussianNB().fit(X_train_scaled, y_train)
-
-gnb_train_score, gnb_test_score, gnb_train_confusion_matrix, gnb_test_confusion_matrix, gnb_results_dict = model_results(gnb, 'Gaussian Naive Bayes')
-
-print 'Mean Training Set Accuracy (based on 5 fold) - Gaussian Naive Bayes: {}'.format(gnb_train_score)
-print 'Test Set Accuracy - Gaussian Naive Bayes: {}'.format(gnb_test_score)
+gnb_train_stats = modelScoring(y_train, y_train_gnb, 'Training', 'Gaussian Naive Bayes', printing = False, table = train_stats)
+gnb_test_stats = modelScoring(y_test, y_test_gnb, 'Testing', 'Gaussian Naive Bayes', printing = False, table = test_stats)
 
 ### Logistic Regression ----------
-lr_mod = LogisticRegression(random_state = 0, max_iter = 10000)
-lr_grid = {'penalty': ['l1', 'l2']
-           , 'C': np.power(10.0, np.arange(-10, 10))}
-lr = GridSearchCV(lr_mod, lr_grid, scoring = 'accuracy')
-lr.fit(X_train_scaled, y_train)
+lrGrid = {'penalty': ['l1', 'l2'],
+          'C': np.linspace(0.01, 10000, 20),
+          'random_state': [SEED]}
 
-lr_train_score, lr_test_score, lr_train_confusion_matrix, lr_test_confusion_matrix, lr_results_dict = model_results(lr, 'Logistic Regression CV')
+model_lr, y_train_lr, y_test_lr = modelFit(LogisticRegression(),
+                                           parameters=lrGrid, whichXTrain=X_train_std, whichXTest=X_test_std, whichYTrain=y_train)
 
-print 'Mean Training Set Accuracy (based on 5 fold) - Logistic Regression CV: {}'.format(lr_train_score)
-print 'Test Set Accuracy - Logistic Regression CV: {}'.format(lr_test_score)
+lr_train_stats = modelScoring(y_train, y_train_lr, 'Training', 'Logistic Regression', printing = False, table = train_stats)
+lr_test_stats = modelScoring(y_test, y_test_lr, 'Testing', 'Logistic Regression', printing = False, table = test_stats)
 
 ### K Nearest Neighbors ----------
-knn_mod = KNeighborsClassifier()
-knn_grid = {'n_neighbors': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 20]
-            , 'weights': ['uniform', 'distance']
-            , 'p': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-knn = GridSearchCV(knn_mod, knn_grid, scoring = 'accuracy')
-knn.fit(X_train_scaled, y_train)
+knnGrid = {'n_neighbors': np.linspace(1, 10, 10).astype(int),
+           'weights': ['uniform', 'distance'],
+           'p': np.linspace(1, 6, 6).astype(int),
+           'algorithm': ['auto']}
 
-knn_train_score, knn_test_score, knn_train_confusion_matrix, knn_test_confusion_matrix, knn_results_dict = model_results(knn, 'K Nearest Neighbors')
+model_knn, y_train_knn, y_test_knn = modelFit(KNeighborsClassifier(),
+                                              parameters=knnGrid, whichXTrain=X_train_minmax, whichXTest=X_test_minmax, whichYTrain=y_train)
 
-print 'Mean Training Set Accuracy (based on 5 fold) - K Nearest Neighbors: {}'.format(knn_train_score)
-print 'Test Set Accuracy - K Nearest Neighbors: {}'.format(knn_test_score)
+knn_train_stats = modelScoring(y_train, y_train_knn, 'Training', 'K Nearest Neighbors', printing = False, table = train_stats)
+knn_test_stats = modelScoring(y_test, y_test_knn, 'Testing', 'K Nearest Neighbors', printing = False, table = test_stats)
 
-### Support Vector Machine ----------
+### Support Vector Machines ----------
+svcGrid = {'C': np.linspace(0.01, 10000, 20),
+           'kernel': ['linear', 'rbf'],
+           'class_weight': ['balanced', None],
+           'random_state': [SEED]}
 
+model_svc, y_train_svc, y_test_svc = modelFit(SVC(),
+                                              parameters=svcGrid, whichXTrain=X_train_std, whichXTest=X_test_std, whichYTrain=y_train)
+
+svc_train_stats = modelScoring(y_train, y_train_svc, 'Training', 'Support Vector Machine', printing = False, table = train_stats)
+svc_test_stats = modelScoring(y_test, y_test_svc, 'Testing', 'Support Vector Machine', printing = False, table = test_stats)
 
 ### Random Forest ----------
+rfGrid = {'n_estimators': [1000],
+          'max_features': np.linspace(2, X_train.shape[1], 8).astype(int),
+          'max_depth': np.linspace(2, 8, 7).astype(int),
+          'class_weight': ['balanced', None],
+          'bootstrap': [True],
+          'oob_score': [True],
+          'random_state': [SEED],
+          'n_jobs': [-1]}
 
+model_rf, y_train_rf, y_test_rf = modelFit(RandomForestClassifier(),
+                                           parameters=rfGrid, whichXTrain=X_train, whichXTest=X_test, whichYTrain=y_train)
 
-### Gradient Boosting Machine ----------
+rf_train_stats = modelScoring(y_train, y_train_rf, 'Training', 'Random Forest', printing = False, table = train_stats)
+rf_test_stats = modelScoring(y_test, y_test_rf, 'Testing', 'Random Forest', printing = False, table = test_stats)
 
+### Gradient Boosting ----------
+gbGrid = {'learning_rate': [0.01],
+          'n_estimators': [500],
+          'max_depth': np.linspace(2, 8, 7).astype(int),
+          'max_features': np.linspace(2, X_train.shape[1], 8).astype(int),
+          'random_state': [SEED]}
+
+model_gb, y_train_gb, y_test_gb = modelFit(GradientBoostingClassifier(),
+                                           parameters=gbGrid, whichXTrain=X_train, whichXTest=X_test, whichYTrain=y_train)
+
+gb_train_stats = modelScoring(y_train, y_train_gb, 'Training', 'Gradient Boosting', printing = False, table = train_stats)
+gb_test_stats = modelScoring(y_test, y_test_gb, 'Testing', 'Gradient Boosting', printing = False, table = test_stats)
 
 ### Neural Network ----------
+model_nn, y_train_nn, y_test_nn = modelFit(MLPClassifier(max_iter=100000, random_state=SEED),
+                                           parameters=None, whichXTrain=X_train_std, whichXTest=X_test_std, whichYTrain=y_train)
 
+nn_train_stats = modelScoring(y_train, y_train_nn, 'Training', 'Neural Network', printing = False, table = train_stats)
+nn_test_stats = modelScoring(y_test, y_test_nn, 'Testing', 'Neural Network', printing = False, table = test_stats)
 
 ### Results Data Frame ----------
-results_final = (pd.DataFrame([dum_results_dict
-                               , gnb_results_dict
-                               , lr_results_dict
-                               , knn_results_dict])
-                 .reindex_axis(['Algorithm'
-                                , 'Accuracy (Train, Test)'
-                                , 'Sensitivity (Train, Test)'
-                                , 'Specificity (Train, Test)'
-                                , 'Positive Predictive Value (Train, Test)'
-                                , 'Negative Predictive Value (Train, Test)'], axis = 1))
-print results_final
+train_fitness_stats = pd.DataFrame(train_stats,
+                                   columns=['Algorithm', 'Accuracy', 'Sensitivity', 'Specificity',
+                                            'Positive Predictive Value', 'Negative Predictive Value',
+                                            'F1 Score', 'ROC AUC Score'])
+test_fitness_stats = pd.DataFrame(test_stats,
+                                  columns=['Algorithm', 'Accuracy', 'Sensitivity', 'Specificity',
+                                           'Positive Predictive Value', 'Negative Predictive Value',
+                                           'F1 Score', 'ROC AUC Score'])
+
+print train_fitness_stats
+print test_fitness_stats
